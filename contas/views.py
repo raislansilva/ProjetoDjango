098626a50a76900ac20e876ec3d2edc5b98b4ae8
raislansilva <raislan6@gmail.com>
2019-04-07 13:42:25 -0300

    commit

---
+++ contas/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1','t2','t3']
+    data['now'] = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'contas/home.html', data)
